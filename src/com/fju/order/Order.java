08a0846207a390c3df1818636593a8f6136c789e
bowwwwen <94377197+bowwwwen@users.noreply.github.com>
2022-04-29 12:12:04 +0800

    mid exam order commit

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -1,11 +1,19 @@
 package com.fju.order;
 
 public class Order {
+    public int shipFeed;
     int amount;
     Delivery delivery;
+
 
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+    shipFeed = delivery.price;
+    }
+
+
+    public int total() {
+        return amount+shipFeed;
     }
 }
